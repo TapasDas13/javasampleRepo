cc4375cbe3839b896ebb04a4c0606fe4956de2e4
Tapas <tapas.diatm@gmail.com>
2019-10-24 23:43:47 +0530

    firstcommit

---
+++ sampleGitProject/src/com/test/Sample.java
@@ -0,0 +1,15 @@
+package com.test;
+
+public class Sample {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is my git project");
+
+	}
+
+}
