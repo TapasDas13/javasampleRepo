fc8604ea8492164d204f8d8022420f2791cfa4a7
Tapas <tapas.diatm@gmail.com>
2019-11-08 14:36:05 +0530

    second commit

--- sampleGitProject/src/com/test/Sample.java
+++ sampleGitProject/src/com/test/Sample.java
@@ -8,8 +8,9 @@ public class Sample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("This is my git project");
+		System.out.println("This is my git project  test");
 
 	}
 
 }
+ 
